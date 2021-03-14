3dbbb2df1f3402a457d4716e188a9745b267f956
olciaa <amardaus@gmail.com>
2021-03-14 15:02:10 +0100

    Move player

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -1,6 +1,11 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleLongProperty;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
@@ -26,14 +31,55 @@ public class Game extends Application {
         pane.getChildren().addAll(playerIcon);
         Scene scene = new Scene(pane,windowSizeX,windowSizeY);
 
+        final double speed = 200;
+        final DoubleProperty playerVelocityX = new SimpleDoubleProperty();
+        final DoubleProperty playerVelocityY = new SimpleDoubleProperty();
+        final LongProperty lastUpdateTime = new SimpleLongProperty();
+        final AnimationTimer gameAnimation = new AnimationTimer() {
+            @Override
+            public void handle(long timestamp) {
+                if(lastUpdateTime.get()>0){
+                    final double elapsedSeconds = (timestamp - lastUpdateTime.get())/1000000000.0;
+                    final double deltaX = elapsedSeconds * playerVelocityX.get();
+                    final double deltaY = elapsedSeconds * playerVelocityY.get();
+                    final double oldX = playerIcon.getTranslateX();
+                    final double oldY = playerIcon.getTranslateY();
+                    final double newX = oldX + deltaX;
+                    final double newY = oldY + deltaY;
+                    playerIcon.setTranslateX(newX);
+                    playerIcon.setTranslateY(newY);
+                }
+                lastUpdateTime.set(timestamp);
+            }
+        };
+        gameAnimation.start();
+
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent keyEvent) {
                 if(keyEvent.getCode() == KeyCode.RIGHT){
-                    playerIcon.setLayoutX(playerIcon.getLayoutX() + 10);
+                    playerVelocityX.set(speed);
                 }
                 else if(keyEvent.getCode() == KeyCode.LEFT){
-                    playerIcon.setLayoutX(playerIcon.getLayoutX() - 10);
+                    playerVelocityX.set(-speed);
+                }
+                else if (keyEvent.getCode() == KeyCode.UP){
+                    playerVelocityY.set(-speed);
+                }
+                else if (keyEvent.getCode() == KeyCode.DOWN){
+                    playerVelocityY.set(speed);
+                }
+            }
+        });
+
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.RIGHT || keyEvent.getCode() == KeyCode.LEFT) {
+                    playerVelocityX.set(0);
+                }
+                else if(keyEvent.getCode() == KeyCode.UP || keyEvent.getCode() == KeyCode.DOWN){
+                    playerVelocityY.set(0);
                 }
             }
         });
